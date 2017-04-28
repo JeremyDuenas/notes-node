58451dd725e8f77e6df02c06142c179955f49659
JeremyDuenas <ssjjhd@gmail.com>
2017-04-27 23:09:26 -0700

    fs and JSON

--- playground/json.js
+++ playground/json.js
@@ -10,3 +10,20 @@
 // var person = JSON.parse(personString);
 // console.log(typeof person);
 // console.log(person);
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync('notes.json',originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+
+var note = JSON.parse(noteString);
+
+console.log(typeof noteString);
+console.log(note.title);
