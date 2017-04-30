81afee538f385ff31a36f4904fd3e6a3aa69f7bf
JeremyDuenas <ssjjhd@gmail.com>
2017-04-29 21:57:44 -0700

    More functions

--- notes.js
+++ notes.js
@@ -2,8 +2,21 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note = {
     title,
     body
@@ -19,7 +32,8 @@ var addNote = (title, body) => {
   if (duplicateNotes.length === 0) {
 
     notes.push(note);
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    saveNotes(notes);
+    return note;
   }
 };
 
